2969c3e9003e0711f72a5058de1b6d21044c7cda
ingy <ingy@3cdc45b3-fa1d-0410-b80e-80e55984e33f>
2006-11-21 05:55:21 +0000

    Added test for template names with paths. And also --post-chomp.

--- tests/run/jemplate2.js
+++ tests/run/jemplate2.js
@@ -9,6 +9,27 @@
 if (typeof(Jemplate) == 'undefined')
     throw('Jemplate.js must be loaded before any Jemplate template files');
 
+Jemplate.templateMap['greetings'] = function(context) {
+    if (! context) throw('Jemplate function called without context\n');
+    var stash = context.stash;
+    var output = '';
+
+    try {
+output += 'English: ';
+//line 1 "greetings"
+output += context.include('greeting/english');
+output += '\nSpanish: ';
+//line 3 "greetings"
+output += context.include('greeting/spanish');
+    }
+    catch(e) {
+        var error = context.set_error(e, output);
+        throw(error);
+    }
+
+    return output;
+}
+
 Jemplate.templateMap['hello'] = function(context) {
     if (! context) throw('Jemplate function called without context\n');
     var stash = context.stash;
@@ -19,6 +40,44 @@ output += 'Hello, ';
 //line 1 "hello"
 output += stash.get('name');
 output += '!\n';
+    }
+    catch(e) {
+        var error = context.set_error(e, output);
+        throw(error);
+    }
+
+    return output;
+}
+
+Jemplate.templateMap['greeting/english'] = function(context) {
+    if (! context) throw('Jemplate function called without context\n');
+    var stash = context.stash;
+    var output = '';
+
+    try {
+output += 'Howdy ';
+//line 1 "greeting/english"
+output += stash.get('name');
+output += '. How are you?\n';
+    }
+    catch(e) {
+        var error = context.set_error(e, output);
+        throw(error);
+    }
+
+    return output;
+}
+
+Jemplate.templateMap['greeting/spanish'] = function(context) {
+    if (! context) throw('Jemplate function called without context\n');
+    var stash = context.stash;
+    var output = '';
+
+    try {
+output += 'Hola ';
+//line 1 "greeting/spanish"
+output += stash.get('name');
+output += '. Como estas?\n';
     }
     catch(e) {
         var error = context.set_error(e, output);
