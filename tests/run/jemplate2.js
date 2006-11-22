d91dd787037549cf361e6016d4389c4060328a48
ingy <ingy@3cdc45b3-fa1d-0410-b80e-80e55984e33f>
2006-11-22 17:14:29 +0000

    The sorting changed this compilation output.

--- tests/run/jemplate2.js
+++ tests/run/jemplate2.js
@@ -9,6 +9,44 @@
 if (typeof(Jemplate) == 'undefined')
     throw('Jemplate.js must be loaded before any Jemplate template files');
 
+Jemplate.templateMap['greeting/english'] = function(context) {
+    if (! context) throw('Jemplate function called without context\n');
+    var stash = context.stash;
+    var output = '';
+
+    try {
+output += 'Howdy ';
+//line 1 "greeting/english"
+output += stash.get('name');
+output += '. How are you?\n';
+    }
+    catch(e) {
+        var error = context.set_error(e, output);
+        throw(error);
+    }
+
+    return output;
+}
+
+Jemplate.templateMap['greeting/spanish'] = function(context) {
+    if (! context) throw('Jemplate function called without context\n');
+    var stash = context.stash;
+    var output = '';
+
+    try {
+output += 'Hola ';
+//line 1 "greeting/spanish"
+output += stash.get('name');
+output += '. Como estas?\n';
+    }
+    catch(e) {
+        var error = context.set_error(e, output);
+        throw(error);
+    }
+
+    return output;
+}
+
 Jemplate.templateMap['greetings'] = function(context) {
     if (! context) throw('Jemplate function called without context\n');
     var stash = context.stash;
@@ -40,44 +78,6 @@ output += 'Hello, ';
 //line 1 "hello"
 output += stash.get('name');
 output += '!\n';
-    }
-    catch(e) {
-        var error = context.set_error(e, output);
-        throw(error);
-    }
-
-    return output;
-}
-
-Jemplate.templateMap['greeting/english'] = function(context) {
-    if (! context) throw('Jemplate function called without context\n');
-    var stash = context.stash;
-    var output = '';
-
-    try {
-output += 'Howdy ';
-//line 1 "greeting/english"
-output += stash.get('name');
-output += '. How are you?\n';
-    }
-    catch(e) {
-        var error = context.set_error(e, output);
-        throw(error);
-    }
-
-    return output;
-}
-
-Jemplate.templateMap['greeting/spanish'] = function(context) {
-    if (! context) throw('Jemplate function called without context\n');
-    var stash = context.stash;
-    var output = '';
-
-    try {
-output += 'Hola ';
-//line 1 "greeting/spanish"
-output += stash.get('name');
-output += '. Como estas?\n';
     }
     catch(e) {
         var error = context.set_error(e, output);
