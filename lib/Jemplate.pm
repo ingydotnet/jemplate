903e1e8f37e8ada2714d1ef0e8ccbf1635b7f0dd
Forest Belton <fbelton@theore.tc>
2014-02-08 16:52:38 -0800

    Fix documentation for lib/Jemplate.pm

--- lib/Jemplate.pm
+++ lib/Jemplate.pm
@@ -1,9 +1,10 @@
-##
-# name:      Jemplate
-# abstract:  JavaScript Templating with Template Toolkit
-# author:    Ingy döt Net <ingy@cpan.org>
-# license:   perl
-# copyright: 2006-2008, 2011
+=encoding utf8
+
+=head1 NAME
+
+Jemplate - JavaScript Templating with Template Toolkit
+
+=cut
 
 # ToDo:
 # - Module::Package
@@ -687,3 +688,18 @@ David A. Coffey <dacoffey@cogsmith.com>
 Robert Krimen <robertkrimen@gmail.com>
 
 Nickolay Platonov <nickolay8@gmail.com>
+
+=head1 AUTHOR
+
+Ingy döt Net <ingy@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2006-2014, 2011. Ingy döt Net.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+See http://www.perl.com/perl/misc/Artistic.html
+
+=cut
